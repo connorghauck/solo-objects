409803b71597915f975bb7ea3248fecd0354e010
Connor Hauck <connorghauck@yahoo.com>
2016-09-21 20:50:18 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# EG - Evil Geniuses
+
+##### I chose this team because I'm a nerd who loves DotA 2.
+
+
+"Evil Geniuses are a DotA 2 team created in 1999 consisting of the players Suma1L, Arteezy, UNiVeRSE, zai, and Cr1t-.
+They have won 6 major tournaments, totalling in over $13,000,000, including the 2015 DotA 2 International. Which alone netted them $6,600,000."
---
+++ script.js
@@ -0,0 +1,18 @@
+var EG = {
+  abbrev: 'Evil Geniuses',
+  mid: 'Suma1L',
+  carry: 'Arteezy',
+  offlaner: 'UNiVeRSE',
+  roamingSupport: 'zai',
+  position5: 'Cr1t-',
+  tournamentsWon: '6',
+  internationalChamps: '2015',
+  tournamentWinnings: '$13,000,000',
+  createdIn: '1999',
+}
+
+
+function evilGeniuses () {
+  var para = EG.abbrev + " are a DotA 2 team created in " + EG.createdIn + " consisting of the players " + EG.mid + ", " + EG.carry + ", " + EG.offlaner + ", " + EG.roamingSupport + ", and " + EG.position5 + ".\nThey have won " + EG.tournamentsWon + " major tournaments, totalling in over " + EG.tournamentWinnings + ", including the " + EG.internationalChamps + " DotA 2 International. Which alone netted them $6,600,000.";
+  return para;
+}
